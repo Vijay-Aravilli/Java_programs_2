f1cf19c36547a9278ed0c680f8baa9149aad1e92
Vijay <vijaypadmakar1999@gmail.com>
2022-11-17 19:45:02 +0530

    First program to print sum of the elements in an array

---
+++ Array_Sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Array_Sum {
+    public static void main(String[]args ){
+        Scanner sc=new Scanner(System.in);
+        int []ar=new int[10];
+        int sum=0;
+        for(int i=0;i<ar.length;i++){
+            ar[i]=sc.nextInt();
+            sum+=ar[i];
+
+        }
+        System.out.println(sum);
+
+    }
+    
+}
